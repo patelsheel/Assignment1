57be69f4f9fca333fb4bf11696d12058f6ec4526
patelsheel <77370692+patelsheel@users.noreply.github.com>
2023-08-27 17:48:30 -0400

    Initial Commit

---
+++ Assignment1/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("Product/{prod_name?}")]
+        public string GetProduct([FromRoute] string prod_name =
+            "Not Avaliable")
+        {
+            return $"{prod_name}";
+        }
+
+        [HttpPost("ProductType")]
+        public string AddProduct(Products product_Type)
+        {
+            return product_Type.prod_type;
+        }
+    }
+}
---
+++ Assignment1/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Assignment1.Models
+{
+	public class Products
+	{
+		public string prod_name { get; set; }
+		public string prod_type { get; set; }
+	}
+}
+
